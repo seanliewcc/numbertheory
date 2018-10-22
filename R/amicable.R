46ec103a6e48cfe13aae56a6208be1b20b29afff
Sean Liew <43155141+seanliewcc@users.noreply.github.com>
2018-10-22 18:31:51 -0400

    Fixed definition

--- R/amicable.R
+++ R/amicable.R
@@ -2,7 +2,7 @@
 #'
 #' Does not have any dependencies, but for assignment purposes will suggest dplyr to be installed.
 #'
-#' Two distinct positive integers are amicable if their sums of their proper divisors are equal.
+#' Two distinct positive integers are amicable if their sum of proper divisors of each are equal to the other number.
 #'
 #' @param x The first number.
 #' @param y The second number.
@@ -16,5 +16,5 @@ amicable <- function(x,y) { # no defaults since the user should input two distin
   if (x == y | x < 1 | y < 1 | x%%1 != 0 | y%%1 != 0) stop ("x and y must be distinct positive integers")
   factors_x <- (1:x)[x %% (1:x) == 0]
   factors_y <- (1:y)[y %% (1:y) == 0]
-  sum(factors_x) == sum(factors_y)
+  sum(factors_x) == y | sum(factors_y) == x
 }
